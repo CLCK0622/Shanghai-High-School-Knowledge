10baa6a82a6009eed29e90bff7fff3541fb5980b
Kevin Zhong <zhongyi070622@163.com>
2022-12-22 16:00:58 +0800

    add 溴和碘的提取

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useRouter } from "next/router"
+import { useConfig } from 'nextra-theme-docs'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>上海高中课本知识整理</span>,
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter, title } = useConfig()
+    return <>
+      <meta property="og:url" content={`https://knowledge.clckblog.space${asPath}`} />
+      <meta property="og:title" content={title + ' | 上海高中课本知识整理'} />
+      <title>{title + ' | 上海高中课本知识整理'}</title>
+      <meta property="og:description" content={frontMatter.description || '上海高中课本知识整理'} />
+    </>
+  },
   project: {
     link: 'https://github.com/CLCK0622/Shanghai-High-School-Knowledge',
   },
