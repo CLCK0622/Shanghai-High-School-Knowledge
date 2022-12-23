c4dd49e75d2b1ac7ed3cba366eeb7b7a7969ea3e
Kevin Zhong <zhongyi070622@163.com>
2022-12-23 08:16:13 +0800

    transl.

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,6 @@ const config: DocsThemeConfig = {
     return <>
       <meta property="og:url" content={`https://knowledge.clckblog.space${asPath}`} />
       <meta property="og:title" content={title + ' | 上海高中课本知识整理'} />
-      <title>{title + ' | 上海高中课本知识整理'}</title>
       <meta property="og:description" content={frontMatter.description || '上海高中课本知识整理'} />
     </>
   },
@@ -21,9 +20,38 @@ const config: DocsThemeConfig = {
   // chat: {
   //   link: 'https://discord.com',
   // },
-  docsRepositoryBase: 'https://github.com/CLCK0622/Shanghai-High-School-Knowledge',
+  docsRepositoryBase: 'https://github.com/CLCK0622/Shanghai-High-School-Knowledge/tree/main',
   footer: {
     text: 'Built with Nextra, modified by Kevin Zhong (aka CLCK) and other contributors. ',
+  },
+  search: {
+    emptyResult: <div className="nx-mt-4 nx-flex nx-justify-center nx-items-center nx-text-gray-500 nx-font-semibold nx-text-s nx-select-none">没有结果</div>,
+    loading: '加载中...',
+    placeholder: '搜索',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 0,
+  },
+  toc: {
+    title: "目录", 
+  },
+  editLink: {
+    text: '编辑本页面 ↗',
+  },
+  feedback: {
+    content: '有问题？前往反馈 ↗',
+  },
+  useNextSeoProps() {
+    const { route } = useRouter()
+    if (route !== '/') {
+      return {
+        titleTemplate: '%s | 上海高中课本知识整理'
+      }
+    } else {
+      return {
+        titleTemplate: '上海高中课本知识整理'
+      }
+    }
   },
 }
 
