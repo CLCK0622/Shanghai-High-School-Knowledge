2511ad3cd8e61e047d5d6dcf0b9ac4f757aa337d
Kevin Zhong <zhongyi070622@163.com>
2022-12-27 19:35:51 +0800

    upd theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,8 +10,7 @@ const config: DocsThemeConfig = {
     const { frontMatter, title } = useConfig()
     return <>
       <meta property="og:url" content={`https://knowledge.clckblog.space${asPath}`} />
-      <meta property="og:title" content={title + ' | 上海高中课本知识整理'} />
-      <meta property="og:description" content={frontMatter.description || '上海高中课本知识整理'} />
+      <meta property="og:description" content={frontMatter.description || '由 @CLCK 及其他人一起整理的，以上海市 2022 年高中课标教材为蓝本的高中课本知识库。'} />
     </>
   },
   project: {
@@ -22,7 +21,7 @@ const config: DocsThemeConfig = {
   // },
   docsRepositoryBase: 'https://github.com/CLCK0622/Shanghai-High-School-Knowledge/tree/main',
   footer: {
-    text: 'Built with Nextra, modified by Kevin Zhong (aka CLCK) and other contributors. ',
+    text: '由 Kevin Zhong (aka CLCK) 和其他贡献者精心整理 | 自豪地使用 Nextra 生成网站',
   },
   search: {
     emptyResult: <div className="nx-mt-4 nx-flex nx-justify-center nx-items-center nx-text-gray-500 nx-font-semibold nx-text-s nx-select-none">没有结果</div>,
@@ -45,7 +44,7 @@ const config: DocsThemeConfig = {
     const { route } = useRouter()
     if (route !== '/') {
       return {
-        titleTemplate: '%s | 上海高中课本知识整理'
+        titleTemplate: '%s - 上海高中课本知识整理'
       }
     } else {
       return {
