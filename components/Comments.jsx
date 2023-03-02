3b5112bc0bef35cb9ef8c66d047b7c44aa392d44
CLCK <zhongyi070622@163.com>
2023-03-02 19:12:24 +0800

    Update Comments.jsx

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,24 +1,29 @@
-import Giscus from '@giscus/react';
+import Giscus from "@giscus/react";
 
 function Comments() {
   return (
-    <Giscus
-    id="comments"
-    repo="CLCK0622/Shanghai-High-School-Knowledge"
-    repoId="R_kgDOIqDhzg"
-    category="Announcements"
-    categoryId="DIC_kwDOIqDhzs4CUl3j"
-    mapping="pathname"
-    strict="0"
-    reactionsEnabled="1"
-    emitMetadata="0"
-    inputPosition="top"
-    theme="preferred_color_scheme"
-    lang="zh-CN"
-    crossorigin="anonymous"
-    loading="lazy"
-    async
-    />
+    <>
+      <div style={{ height: 50 }} />
+      <hr />
+      <div style={{ height: 50 }} />
+      <Giscus
+        id="comments"
+        repo="CLCK0622/Shanghai-High-School-Knowledge"
+        repoId="R_kgDOIqDhzg"
+        category="Announcements"
+        categoryId="DIC_kwDOIqDhzs4CUl3j"
+        mapping="pathname"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="preferred_color_scheme"
+        lang="zh-CN"
+        crossorigin="anonymous"
+        loading="lazy"
+        async
+      />
+    </>
   );
 }
 
