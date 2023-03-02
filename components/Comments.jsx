75523f4fc2d2e0f744c983911ac6d7e305f7267b
CLCK <zhongyi070622@163.com>
2023-03-02 17:11:58 +0800

    add giscus

---
+++ components/Comments.jsx
@@ -0,0 +1,25 @@
+import Giscus from '@giscus/react';
+
+function Comments() {
+  return (
+    <Giscus
+    id="comments"
+    repo="CLCK0622/Shanghai-High-School-Knowledge"
+    repoId="R_kgDOIqDhzg"
+    category="Announcements"
+    categoryId="DIC_kwDOIqDhzs4CUl3j"
+    mapping="pathname"
+    strict="0"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    theme="preferred_color_scheme"
+    lang="zh-CN"
+    crossorigin="anonymous"
+    loading="lazy"
+    async
+    />
+  );
+}
+
+export default Comments;
